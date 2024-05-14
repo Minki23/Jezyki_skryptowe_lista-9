44a5e58a82fd68ab7cadca5432049ef71c0fd956
nataliaidk <127215791+nataliaidk@users.noreply.github.com>
2024-05-14 17:26:39 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,30 +1,52 @@
 import pytest
 import datetime
 from ipaddress import IPv4Address
-from OOP_Python import SSHLogJournal, SSHLogEntry, PasswordRejected, PasswordAccepted, Error, OtherInfo
+from OOP_Python import SSHLogJournal, PasswordRejected, PasswordAccepted, Error, OtherInfo
 
-#uruchomienie : pystest -v tests.py
-#jebana klasa abstrakyjna nei ograniam tego :((( totalnie nie wiem jak zrobic instancje zeby to sprawdzic; trzeb zrobic testy dla czasu i dla adresow ipv4
+#testowanie czy dobrze pobieramy czas z loga
 class TestSSHLogEntryTime:
     def test_extract_time(self):
-        entry = SSHLogEntry("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
-        assert entry.time == datetime.datetime(2024, 12, 10, 6, 55, 48)
+        journal = SSHLogJournal()
+        journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
+        assert journal[0].time == datetime.datetime(2024, 12, 10, 6, 55, 48)
 
+#ipv4 - dobry, zly i zaden
 class TestSSHLogEntry:
     def test_get_ipv4_address_valid(self):
-        entry = SSHLogEntry("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
-        assert entry.get_ipv4_address() == IPv4Address("173.234.31.186")
+        journal = SSHLogJournal()
+        journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
+        assert journal[0].get_ipv4_address() == IPv4Address("173.234.31.186")
 
     def test_get_ipv4_address_invalid(self):
-        entry = SSHLogEntry("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 666.777.88.213 port 38926 ssh2")
-        assert entry.get_ipv4_address() is None
+        journal = SSHLogJournal()
+        journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 666.777.88.213 port 38926 ssh2")
+        assert journal[0].get_ipv4_address() == None
 
     def test_get_ipv4_address_none(self):
-        entry = SSHLogEntry("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from port 38926 ssh2")
-        assert entry.get_ipv4_address() is None
+        journal = SSHLogJournal()
+        journal.append("Dec 10 06:55:48 LabSZ sshd[24200]: invalid user webmaster [preauth]")
+        assert journal[0].get_ipv4_address() == None
 
 
-#przechodzi
+
+    
+
+#testy metody append z dekoratorem dla wszystkich klas pochodnych
+@pytest.mark.parametrize("content, expected_type", [
+    ("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password. for uucp from 103.207.39.212 port 51528 ssh2", PasswordRejected),
+    ("Dec 10 06:55:48 LabSZ sshd[24200]: Accepted password. for valid user admin from 173.234.31.186 port 38926 ssh2", PasswordAccepted),
+    ("Dec 10 06:55:48 LabSZ sshd[24200]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]", Error),
+    ("Dec 10 06:55:48 LabSZ sshd[24200]: kotki sa super slodkie i kochane aa", OtherInfo),
+])
+def test_journal_append(content, expected_type):
+    journal = SSHLogJournal()
+    journal.append(content)
+    assert journal[0] == expected_type(content)
+
+
+
+#tu sobie po orpstu napsialam testy zeby sprawdzic czy kazda z tych funkcji dziala - dziala
+
 class TestPasswordRejected:
     def test_validate_true(self):
         entry = PasswordRejected("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
@@ -34,7 +56,6 @@ class TestPasswordRejected:
         entry = PasswordRejected("Dec 10 06:55:48 LabSZ sshd[24200]: Accepted password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
         assert entry.validate() is False
 
-#przechodzi        
 class TestPasswordAccepted:
     def test_validate_true(self):
         entry = PasswordAccepted("Dec 10 06:55:48 LabSZ sshd[24200]: Accepted password for valid user admin from 173.234.31.186 port 38926 ssh2")
@@ -42,9 +63,7 @@ class TestPasswordAccepted:
     def test_validate_false(self):
         entry = PasswordAccepted("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2")
         assert entry.validate() is False
-
-
-#  nie przechodzi      
+    
 class TestError:
     def test_validate_true(self):
         entry = Error("Dec 10 06:55:48 LabSZ sshd[24200]: error occurred while processing request")
@@ -56,18 +75,3 @@ class TestOtherInfo:
     def test_validate_true(self):
         entry = OtherInfo("Dec 10 06:55:48 LabSZ sshd[24200]: Some other info message")
         assert entry.validate() is True
-    def test_validate_true(self):
-        entry = OtherInfo("Dec 10 06:55:48 LabSZ sshd[24200]: Error")
-        assert entry.validate() is False
-
-#przechodzi tylko other reszta nie
-@pytest.mark.parametrize("content, expected_type", [
-    ("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for uucp from 103.207.39.212 port 51528 ssh2", PasswordRejected),
-    ("Dec 10 06:55:48 LabSZ sshd[24200]: Accepted password for valid user admin from 173.234.31.186 port 38926 ssh2", PasswordAccepted),
-    ("Dec 10 06:55:48 LabSZ sshd[24200]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]", Error),
-    ("Dec 10 06:55:48 LabSZ sshd[24200]: kotki sa super slodkie", OtherInfo),
-])
-def test_journal_append(content, expected_type):
-    journal = SSHLogJournal()
-    journal.append(content)
-    assert isinstance(journal.logs[0], expected_type)
